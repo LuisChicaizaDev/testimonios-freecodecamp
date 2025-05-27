c44e9dbe2ac57944979aebf0d0e0e7fd0b70e945
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-05-27 15:16:53 +0200

    update: testimonio componente de clase

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import '../stylesheets/Testimonio.css';
 
-/*
-Creamos un componente funcional que recibirá 
-propiedades 'props'
-*/
-function Testimonio(props){
-  return (
-    //Contenedor principal
-    <div className='contenedor-testimonio'>
-      <img 
-        className='img-testimonio'
-        src={require(`../img/avatar-${props.imagen}.jpg`)} //con comillas invertidas
-        alt= {`Avatar de ${props.alt}`}
-      />
+/**
+ * Creamos un componente de clase
+ * Agregamos 'this' para acceder al objeto de propiedades
+ */
+class Testimonio extends React.Component {
 
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>
-          <strong>{props.nombre}</strong> en {props.pais}
-        </p>
-        <p className='cargo-testimonio'>
-          {props.cargo} en <strong>{props.empresa}</strong>
-        </p>
-        <p className='texto-testimonio'>
-          "{props.testimonio}"
-        </p>
+  render() {
+    return (
+      //Contenedor principal
+      <div className='contenedor-testimonio'>
+        <img 
+          className='img-testimonio'
+          src={require(`../img/avatar-${this.props.imagen}.jpg`)} //con comillas invertidas
+          alt= {`Avatar de ${this.props.alt}`}
+        />
+
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>
+            <strong>{this.props.nombre}</strong> en {this.props.pais}
+          </p>
+          <p className='cargo-testimonio'>
+            {this.props.cargo} en <strong>{this.props.empresa}</strong>
+          </p>
+          <p className='texto-testimonio'>
+            "{this.props.testimonio}"
+          </p>
+        </div>
+
       </div>
+    );
+  }
 
-    </div>
-
-  );
 }
 
 //exportamos el componente
